476351b699316d7f31425dc50d50ae58efe5d068
jd120219 <quoc.huynh1@my.jcu.edu.au>
2025-09-23 16:01:10 +1000

    Refactor password program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,10 +2,24 @@
 
 MINIMUM_CHARACTERS = 5
 
-password = input("Enter password of at least 5 characters:")
 
-while len(password) < MINIMUM_CHARACTERS:
-    print("Password must be at least 5 characters")
+def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+def get_password():
+    """Get password and validate that it is at least 5 characters long"""
     password = input("Enter password of at least 5 characters:")
-for i in range(len(password)):
-    print("*", end="")
+    while len(password) < MINIMUM_CHARACTERS:
+        print("Password must be at least 5 characters")
+        password = input("Enter password of at least 5 characters:")
+    return password
+
+
+def print_asterisks(password):
+    for i in range(len(password)):
+        print("*", end="")
+
+
+main()
