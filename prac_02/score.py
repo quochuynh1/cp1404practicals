938dc73045dd128ad2618b6e5d8c29c0e5f3c62c
jd120219 <quoc.huynh1@my.jcu.edu.au>
2025-09-24 15:29:11 +1000

    Refactor score.py to take a random score and give it a result

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,20 +3,23 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
+import random
+
 
 def main():
     """Get score from user and print the category of score (result)"""
-    score = float(input("Enter score: "))
-    print("Your score is:", categorise_score(score))
+    random_score = random.randint(0, 100)
+    print("Your random score was: ", random_score, "and \n")
+    print("Your random score is:", categorise_score(random_score))
 
 
-def categorise_score(score):
+def categorise_score(random_score):
     """Sort the given score (parameter) into categories with error checking"""
-    if score < 0 or score > 100:
+    if random_score < 0 or random_score > 100:
         return "Invalid Score"
-    elif score >= 90:
+    elif random_score >= 90:
         return "excellent"
-    elif score >= 50:
+    elif random_score >= 50:
         return "passable"
     else:
         return "bad"
