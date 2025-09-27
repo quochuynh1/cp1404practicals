4e6926679d7df6fbecd5bae1651cbcfad36da7f8
jd120219 <quoc.huynh1@my.jcu.edu.au>
2025-09-27 10:42:43 +1000

    Clean up more_scores.py and add meaningful docstrings

--- prac_02/more_scores.py
+++ prac_02/more_scores.py
@@ -1,10 +1,10 @@
-"""Docstring"""
+"""Program to print random results based on number of scores given by user"""
 
 import random
 
 
 def main():
-    """"""
+    """print the random score and result"""
     number_of_scores = get_number_of_scores()
     for i in range(number_of_scores):
         random_score = assign_random_score(number_of_scores)
@@ -12,23 +12,21 @@ def main():
 
 
 def get_number_of_scores():
-    """"""
+    """ask the user how many scores they would like to print"""
     number_of_scores = int(input("Number of Scores:"))
     return number_of_scores
 
 
 def assign_random_score(number_of_scores):
-    """"""
+    """assign however many scores are needed with a random score"""
     for i in range(number_of_scores):
         random_score = random.randint(0, 100)
     return random_score
 
 
 def categorise_score(random_score):
-    """Sort the given score (parameter) into categories with error checking"""
-    if random_score < 0 or random_score > 100:
-        return "Invalid Score"
-    elif random_score >= 90:
+    """Sort the random score into categories with error checking"""
+    if random_score >= 90:
         return "Excellent"
     elif random_score >= 50:
         return "Passable"
