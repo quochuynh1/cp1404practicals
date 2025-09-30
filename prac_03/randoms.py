f1a8a720d4da69331e35411c8c32a87d7bb0b9c4
jd120219 <quoc.huynh1@my.jcu.edu.au>
2025-09-30 13:57:44 +1000

    Add randoms.py and string_formatting.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,12 @@
+# What did you see on line 1?
+# I saw that each time I pressed enter, with print(random.randint(5,20)), it would print a random integer ranging from 5 to 20. The smallest number I could've seen was 5 while the largest was 20.
+
+# What did you see on line 2?
+# I saw that each time I pressed enter, with print(random.randrange(3, 10, 2)), it would print a random integer in the range from 3 to 10 in steps of 2. Therefore, the possible outputs would be 3, 5, 7, 9 and 4 would not be produced.
+
+# What did you see on line 3
+# I saw that each time I pressed enter, with print(random.uniform(2.5, 5.5)), it would print a float ranging from 2.5 to 5.5. The smallest number I could've seen was 2.5 while the largest was 5.5.
+
+import random
+print(random.randint(0,100))
+
