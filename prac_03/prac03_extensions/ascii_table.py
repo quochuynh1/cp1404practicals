77d1efb230b3dfacf42ae88d264fd1a5d0914005
jd120219 <quoc.huynh1@my.jcu.edu.au>
2025-10-03 17:02:18 +1000

    Refactor ascii_table.py so that try except properly catches the TypeError and ValueError

--- prac_03/prac03_extensions/ascii_table.py
+++ prac_03/prac03_extensions/ascii_table.py
@@ -1,11 +1,14 @@
 """Program to convert from string to ASCII and vice versa"""
 
-import random
+LOWER = 33
+UPPER = 127
+
+
 
 def main():
     is_valid_character = False
-    # character = input("Enter a character: ")
-    character = "a"
+    character = input("Enter a character: ")
+    # character = "a"
     while not is_valid_character:
         try:
             ascii_conversion = convert_character_to_ascii(character)
@@ -15,29 +18,30 @@ def main():
             print("Must be a valid character")
             character = input("Enter a character: ")
 
-    number = int(input("Enter a number between 33 and 127: "))
-    # number = random.randint(33, 127)
-    while number < 33 or number > 127:
-        print("Invalid input")
-        number = int(input("Enter a number between 33 and 127: "))
-    try:
-        integer_conversion = convert_integer_to_character(number)
-        print(f"The character of '{number}' is '{integer_conversion}'")
-    # except TypeError:
-    #     print("Must be between 33 and 127")
-    except ValueError:
-        print("Must be an integer")
-
-
+    is_valid_number = False
+    while not is_valid_number:
+        try:
+            number = int(input(f"Enter a number between {LOWER} and {UPPER}: "))
+            integer_conversion = convert_integer_to_character(number)
+            print(f"The character for '{number}' is '{integer_conversion}'")
+            is_valid_number = True
+        except TypeError:
+            print("Must be between 33 and 127")
+        except ValueError:
+            print("Must be an integer")
 
 
 def convert_character_to_ascii(character):
     ascii_value = ord(character)
     return ascii_value
 
+
 def convert_integer_to_character(number):
-    integer_as_character = chr(number)
-    return integer_as_character
+    if number < LOWER or number > UPPER:
+        raise TypeError("Number out of range")
+    else:
+        integer_as_character = chr(number)
+        return integer_as_character
 
 
 main()
