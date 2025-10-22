65ce88d992b326186c842f9bb749837ba5e6d9b6
jd120219 <quoc.huynh1@my.jcu.edu.au>
2025-10-23 09:52:57 +1000

    Complete "Playing the Guitars" prac 6 activity

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -11,6 +11,9 @@ class Guitar:
     def __str__(self):
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
+    def __repr__(self):
+        return str(self)
+
     def get_age(self):
         return CURRENT_YEAR - self.year
 
