9d2901d87bd094858904a5c949391a66e70ec9ae
jd120219 <quoc.huynh1@my.jcu.edu.au>
2025-10-22 17:03:06 +1000

    Complete prac 6 intermediate exercises

---
+++ prac_06/programming_language.py
@@ -0,0 +1,25 @@
+"""
+Prac 06 - Programming Language
+Estimated: 1hr
+Actual:
+"""
+
+
+
+class ProgrammingLanguage:
+   def __init__(self, name="", typing="", reflection=False, year=0):
+       self.name = name
+       self.typing = typing
+       self.reflection = reflection
+       self.year = year
+
+   def __str__(self):
+       return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+   def is_dynamic(self):
+       return self.typing == "Dynamic"
+
+   def __repr__(self):
+       return str(self)
+
+
