455e2ca1e2d9aceb460967f4c9c9bc3c0b61c685
jd120219 <quoc.huynh1@my.jcu.edu.au>
2025-10-27 10:34:42 +1000

    Complete prac 04 extension word generator exercise.

--- prac_04/prac04_extensions/word_generator.py
+++ prac_04/prac04_extensions/word_generator.py
@@ -10,12 +10,33 @@ import random
 VOWELS = "aeiou"
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
 
-word_format = input("Enter a sequence of vowels and constants (e.g., ccvcvvc): ")
-word = ""
-for kind in word_format:
-    if kind == "c":
-        word += random.choice(CONSONANTS)
-    else:
-        word += random.choice(VOWELS)
 
-print(word)
+def main():
+    word_format = is_valid_format()
+    word = ""
+    for kind in word_format:
+        if kind == "c":
+            word += random.choice(CONSONANTS)
+        else:
+            word += random.choice(VOWELS)
+
+    print(word)
+
+
+def is_valid_format():
+    is_valid_input = False
+    while not is_valid_input:
+        try:
+            word_format = input("Enter a sequence of vowels and constants (e.g., ccvcvvc): ")
+            if all(kind in ('c', 'v') for kind in word_format):
+                return word_format
+            else:
+                print("Invalid Input")
+        except ValueError:
+            print("Enter either c or v")
+    return None
+
+
+
+
+main()
