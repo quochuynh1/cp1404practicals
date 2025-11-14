1195a037302fd5ff478c1ac9176661050e2f554e
jd120219 <quoc.huynh1@my.jcu.edu.au>
2025-11-14 10:21:12 +1000

    Add function docstrings

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -8,6 +8,7 @@ from kivy.properties import StringProperty
 
 MILES_TO_KM = 1.60934
 
+
 class MilesConverter(App):
     """ MilesConverterApp is a Kivy App for converting miles to kilometres """
 
@@ -20,6 +21,7 @@ class MilesConverter(App):
         return self.root
 
     def get_valid_integer(self):
+        """Set output result to 0.0 if text entered is not a valid number"""
         try:
             value = float(self.root.ids.input_miles.text)
             return value
@@ -27,17 +29,19 @@ class MilesConverter(App):
             return 0
 
     def handle_conversion(self):
+        """Convert the text input from input_miles to kilometers and print result in output label. """
         value = self.get_valid_integer()
         result = float(value) * MILES_TO_KM
         self.root.ids.output_label.text = str(result)
 
     def handle_increment(self, value):
+        """Increase or decrease the text input by 1."""
         current_miles = self.get_valid_integer()
         new_miles = current_miles + value
         self.root.ids.input_miles.text = str(new_miles)
 
-    def handle_update(self):
-        self.message = self.root.ids.input_miles.text
+    # def handle_update(self):
+    #     self.message = self.root.ids.input_miles.text
 
 
 MilesConverter().run()
